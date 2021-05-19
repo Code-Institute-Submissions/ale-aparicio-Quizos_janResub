19753239ddd20167005be522782310a454a3a521
ale-aparicio <ale.aparicio@live.com>
2021-05-19 00:07:18 +0000

    add

--- quizos/assets/js/script.js
+++ quizos/assets/js/script.js
@@ -1,28 +1,30 @@
 //Slider
+window.onload=function(){
+    const slider = document.querySelector(".slider");
+    const nextBtn = document.querySelector(".next-btn");
+    const prevBtn = document.querySelector(".prev-btn");
+    const slides = document.querySelectorAll(".slide");
+    const slideIcons = document.querySelectorAll(".slide-icon");
+    const numberOfSlides = slides.length;
+    var slideNumber = 0;
 
-const slider = document.querySelector(".slider");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const slides = document.querySelectorAll(".slide");
-const slideIcons = document.querySelectorAll(".slide-icon");
-const numberOfSlides = slides.length;
-var slideNumber = 0;
+    //image slider next button
+    nextBtn.addEventListener("click", () => {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove("active");
+        });
 
-//image slider next button
- nextBtn.addEventListener("click", () => {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-    slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active");
-    });
+        slideNumber++;
 
-    slideNumber++;
+        if(slideNumber > (numberOfSlides - 1)){
+            slideNumber = 0;
+        }
 
-    if(slideNumber > (numberOfSlides - 1)){
-        slideNumber = 0;
-    }
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+        });
 
-    slides[slideNumber].classList.add("active");
-    slideIcons[slideNumber].classList.add("active");
-    });
+  }
