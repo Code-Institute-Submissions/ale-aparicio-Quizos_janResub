9f5b8a6651e6a3c4856995ad97074ffc4e4ad561
ale-aparicio <ale.aparicio@live.com>
2021-05-19 00:55:39 +0000

    add

--- quizos/assets/js/script.js
+++ quizos/assets/js/script.js
@@ -79,6 +79,87 @@ window.onload=function(){
         slider.addEventListener("mouseout", () => {
           repeater();
         });
+
+        //SLIDER ENT 
+        const sliderEnt = document.querySelector(".slider-ent");
+        const nextBtnEnt = document.querySelector(".next-btn-ent");
+        const prevBtnEnt = document.querySelector(".prev-btn-ent");
+        const slidesEnt = document.querySelectorAll(".slide-ent");
+        const slideIconsEnt = document.querySelectorAll(".slide-icon-ent");
+        const numberOfSlidesEnt = slidesEnt.length;
+        var slideNumberEnt = 0;
+
+        //image slider next button
+        nextBtnEnt.addEventListener("click", () => {
+            slidesEnt.forEach((slideEnt) => {
+                slideEnt.classList.remove("active");
+            });
+            slideIconsEnt.forEach((slideIconEnt) => {
+                slideIconEnt.classList.remove("active");
+            });
+
+            slideNumberEnt++;
+
+            if(slideNumberEnt > (numberOfSlidesEnt - 1)){
+                slideNumberEnt = 0;
+            }
+
+            slidesEnt[slideNumberEnt].classList.add("active");
+            slideIconsEnt[slideNumberEnt].classList.add("active");
+            });
+
+        //image slider previous button
+        prevBtnEnt.addEventListener("click", () => {
+            slidesEnt.forEach((slideEnt) => {
+                slideEnt.classList.remove("active");
+            });
+            slideIconsEnt.forEach((slideIconEnt) => {
+                slideIconEnt.classList.remove("active");
+            });
+
+            slideNumberEnt--;
+
+            if(slideNumberEnt < 0){
+                slideNumberEnt = numberOfSlidesEnt - 1;
+            }
+
+            slidesEnt[slideNumberEnt].classList.add("active");
+            slideIconsEnt[slideNumberEnt].classList.add("active");
+            });
+
+            //autoplay
+            var playSliderEnt;
+
+            var repeater = () => {
+              playSliderEnt = setInterval(function(){
+                slidesEnt.forEach((slideEnt) => {
+                  slideEnt.classList.remove("active");
+              });
+              slideIconsEnt.forEach((slideIconEnt) => {
+                  slideIconEnt.classList.remove("active");
+              });
+
+              slideNumberEnt++;
+
+              if(slideNumberEnt > (numberOfSlidesEnt - 1)){
+                  slideNumberEnt = 0;
+              }
+
+              slidesEnt[slideNumberEnt].classList.add("active");
+              slideIconsEnt[slideNumberEnt].classList.add("active");
+              }, 4000);
+            }
+            repeater();
+
+            //stop autoplay on mouseover
+            sliderEnt.addEventListener("mouseover", () => {
+              clearInterval(playSliderEnt);
+            });
+
+            //start autoplay again on mouseout
+            sliderEnt.addEventListener("mouseout", () => {
+              repeater();
+            });
   }
 
 
