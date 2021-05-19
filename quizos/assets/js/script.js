35276c1f519c6a8247cb451aea221c98da13cd30
ale-aparicio <ale.aparicio@live.com>
2021-05-19 00:31:20 +0000

    add

--- quizos/assets/js/script.js
+++ quizos/assets/js/script.js
@@ -46,38 +46,39 @@ window.onload=function(){
         slideIcons[slideNumber].classList.add("active");
         });
 
+        //autoplay
+        var playSlider;
+
+        var repeater = () => {
+          playSlider = setInterval(function(){
+            slides.forEach((slide) => {
+              slide.classList.remove("active");
+          });
+          slideIcons.forEach((slideIcon) => {
+              slideIcon.classList.remove("active");
+          });
+
+          slideNumber++;
+
+          if(slideNumber > (numberOfSlides - 1)){
+              slideNumber = 0;
+          }
+
+          slides[slideNumber].classList.add("active");
+          slideIcons[slideNumber].classList.add("active");
+          }, 4000);
+        }
+        repeater();
+
+        //stop autoplay on mouseover
+        slider.addEventListener("mouseover", () => {
+          clearInterval(playSlider);
+        });
+
+        //start autoplay again on mouseout
+        slider.addEventListener("mouseout", () => {
+          repeater();
+        });
   }
 
-  //image slider autoplay
-  var playSlider;
 
-  var repeater = () => {
-    playSlider = setInterval(function(){
-      slides.forEach((slide) => {
-        slide.classList.remove("active");
-      });
-      slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active");
-      });
-
-      slideNumber++;
-
-      if(slideNumber > (numberOfSlides - 1)){
-        slideNumber = 0;
-      }
-
-      slides[slideNumber].classList.add("active");
-      slideIcons[slideNumber].classList.add("active");
-    }, 4000);
-  }
-  repeater();
-
-  //stop autoplay on mouseover
-  slider.addEventListener("mouseover", () => {
-    clearInterval(playSlider);
-  });
-
-  //start autoplay again on mouseout
-  slider.addEventListener("mouseout", () => {
-    repeater();
-  });
