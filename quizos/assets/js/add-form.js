f2a4e2d9e8dc25d02ed22cc98d02e98a8e6b131f
ale-aparicio <ale.aparicio@live.com>
2021-05-21 14:01:21 +0000

    add

--- quizos/assets/js/add-form.js
+++ quizos/assets/js/add-form.js
@@ -1,3 +1,21 @@
 function addForm() {
     $('#first-form').clone().find('input').val('').end().appendTo('#extra-question');
 };
+
+window.onload=function(){
+
+    function incrementQuestionNum() {
+        let questionNum = parseInt(document.getElementById("first-form").value)
+        let calculatedQuestionNum = calculateQuestionNum();
+        let  = questionNum === calculatedQuestionNum[0];
+
+        let newQuestion = parseInt(document.getElementById("question-num").innerText)
+        document.getElementById("question-num").innerText = ++newQuestion;
+    }
+
+    if(addForm) {
+        incrementQuestionNum()
+    } 
+
+    incrementQuestionNum(calculatedQuestionNum[1]);
+}
