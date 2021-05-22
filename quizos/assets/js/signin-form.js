8b2e3692cc8511aaf63d555bbb1973ec91079753
ale-aparicio <ale.aparicio@live.com>
2021-05-22 01:34:50 +0000

    add

--- quizos/assets/js/signin-form.js
+++ quizos/assets/js/signin-form.js
@@ -4,4 +4,16 @@ window.onload=function(){
         document.querySelector('.popup-s').style.display = 'flex';
     });
 
+    document.querySelector('#exit-btn').addEventListener('click', function(){
+        document.querySelector(".popup-s").style.display = 'none';
+    })
+
+    document.getElementById('log-in').addEventListener('click', function(){
+        document.querySelector('.popup-l').style.display = 'flex';
+    });
+
+    document.querySelector('#exit-btn-2').addEventListener('click', function(){
+        document.querySelector(".popup-l").style.display = 'none';
+    })
+
 }
