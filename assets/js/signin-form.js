8a4281f9e465251401775bcecf82547a83ac843e
ale-aparicio <ale.aparicio@live.com>
2021-05-23 14:12:50 +0000

    add

--- assets/js/signin-form.js
+++ assets/js/signin-form.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+
     
     document.getElementById('sign-up').addEventListener('click', function(){
         document.querySelector('.popup-s').style.display = 'flex';
@@ -16,4 +16,18 @@ window.onload=function(){
         document.querySelector(".popup-l").style.display = 'none';
     })
 
-}
+    document.getElementById('sign-up-f').addEventListener('click', function(){
+        document.querySelector('.popup-s-f').style.display = 'flex';
+    });
+
+    document.querySelector('#exit-btn').addEventListener('click', function(){
+        document.querySelector(".popup-s-f").style.display = 'none';
+    })
+
+    document.getElementById('log-in-f').addEventListener('click', function(){
+        document.querySelector('.popup-l-f').style.display = 'flex';
+    });
+
+    document.querySelector('#exit-btn-2').addEventListener('click', function(){
+        document.querySelector(".popup-l-f").style.display = 'none';
+    })
