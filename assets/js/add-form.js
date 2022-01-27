baf1a00e104ae1b67a4547674511563e209ec4be
ale-aparicio <ale.aparicio@live.com>
2022-01-27 23:14:21 +0000

    Fixed Console errors and removed Unused assets

--- assets/js/add-form.js
+++
@@ -1,8 +0,0 @@
-function addForm() {
-    $('#last-form').clone().find('input').val('').end().appendTo('#extra-question');
-};
-
-function removeForm() {
-    $('#extra-question .question-form').last().remove();
-};
-
